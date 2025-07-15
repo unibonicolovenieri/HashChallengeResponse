cbaa8b4939df7824dcf994d51c5133d87469c8ca
unibonicolovenieri <nicolo.venieri2@studio.unibo.it>
2025-07-15 10:25:38 +0200

    Corretto protocollo

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,21 @@ public class Main {
         Bob bob = new Bob(presharedSecret);
         Channel channel = new Channel();
 
+        System.out.println(
+                """
+                                                                                          \s
+                                     C H A L L E N G E   ↔   R E S P O N S E   ↔   H A S H  \s
+                                -------------------------------------------------------------
+                        """
+        );
+
         // Step 1: Bob → Alice
         String nonceB = bob.generateNonce();
         channel.send("CHALLENGE||" + nonceB);
         System.out.println("Bob → Alice: CHALLENGE " + nonceB);
+
+        System.out.println("--------------------");
+        Thread.sleep(1000);
 
         // Step 2: Alice → Bob
         String received = channel.receive();
@@ -19,7 +30,10 @@ public class Main {
         String nonceA = alice.generateNonce();
         String responseAliceHash = alice.generateResponse(bob.getIdentity());
         channel.send("RESPONSE_A||" + nonceA + "||" + responseAliceHash);
-        System.out.println("Alice → Bob: RESPONSE_A " + nonceA + " + HASH");
+        System.out.println("Alice → Bob: RESPONSE_A " + nonceA + " + " + responseAliceHash);
+
+        System.out.println("--------------------");
+        Thread.sleep(1000);
 
         // Step 3: Bob verifica Alice
         String[] responseParts = channel.receive().split("\\|\\|");
@@ -30,14 +44,22 @@ public class Main {
         boolean aliceOK = bob.verifyResponse("Alice", hashFromAlice);
         System.out.println("Bob verifica Alice: " + (aliceOK ? "OK" : "FALLITO"));
 
+        System.out.println("--------------------");
+        Thread.sleep(1000);
+
         // Step 4: Bob → Alice
         String responseBob = bob.generateResponse("Alice");
         channel.send("RESPONSE_B||" + responseBob);
-        System.out.println("Bob → Alice: RESPONSE_B " + responseBob);
+        System.out.println("Bob → Alice: RESPONSE_B " + responseBob); //HASH INTERO INVIATO
+
+        System.out.println("--------------------");
+        Thread.sleep(1000);
 
         // Step 5: Alice verifica Bob
         String hashFromBob = channel.receive().split("\\|\\|")[1];
         boolean bobOK = alice.verifyResponse(bob.getIdentity(), hashFromBob);
         System.out.println("Alice verifica Bob: " + (bobOK ? "OK" : "FALLITO"));
+
+        System.out.println("--------------------");
     }
 }
